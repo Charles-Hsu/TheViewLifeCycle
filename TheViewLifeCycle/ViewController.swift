83f39a69c213c2b0b222b62e8cff99483ec60b76
Charles Hsu <charles@6e.com.tw>
2015-01-30 18:41:08 +0800

    first commit

---
+++ TheViewLifeCycle/ViewController.swift
@@ -0,0 +1,49 @@
+//
+//  ViewController.swift
+//  TheViewLifeCycle
+//
+//  Created by Charles Hsu on 1/30/15.
+//  Copyright (c) 2015 Loxoll. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+    println("viewDidLoad")
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+    println("didReceiveMemoryWarning")
+  }
+
+  override func viewWillDisappear(animated: Bool) {
+    println("viewWillDisappear")
+  }
+  
+  override func viewDidDisappear(animated: Bool) {
+    println("viewDidDisappear")
+  }
+  
+  override func viewWillAppear(animated: Bool) {
+    println("viewWillAppear")
+  }
+  
+  override func viewWillLayoutSubviews() {
+    println("viewWillLayoutSubviews")
+  }
+  
+  override func viewDidLayoutSubviews() {
+    println("viewDidLayoutSubviews")
+  }
+  override func viewDidAppear(animated: Bool) {
+    println("viewDidAppear")
+  }
+
+}
+
